43c12d44a254de04d131295d9f203797844a3723
yeji-star <jjjsk134@gmail.com>
2025-03-19 14:09:43 +0900

    03_19

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -22,27 +22,48 @@ public class Calc {
         boolean com = plus && multi;
 
         if (com) {
+
             String[] bits = exp.split(" \\+ ");
 
-            String newExp = Arrays.stream(bits)
-                    .mapToInt(Calc::run)
-                    .mapToObj( e -> e + "")
-                    .collect(Collectors.joining(" + "));
-
-            return run(newExp);
-
-//            if (bits.length == 3) {
-//                if (bits[0].contains("*")) {
-//                    return run(bits[0]) + Integer.parseInt(bits[1]) + run(bits[2]);
-//                } else {
-//                    return Integer.parseInt(bits[0]) + Integer.parseInt(bits[1]) + run(bits[2]);
-//                }
+//            String newExp = Arrays.stream(bits)
+//                    .mapToInt(Calc::run)
+//                    .mapToObj( e -> e + "")
+//                    .collect(Collectors.joining(" + "));
 //
-//            }
-//            return Integer.parseInt(bits[0]) + run(bits[1]);
+//            return run(newExp);
+
+            if (bits.length == 3) {
+                if (bits[0].contains("*")) {
+                    return run(bits[0]) + Integer.parseInt(bits[1]) + run(bits[2]);
+                } else {
+                    return Integer.parseInt(bits[0]) + Integer.parseInt(bits[1]) + run(bits[2]);
+                }
+
+            }
+            return Integer.parseInt(bits[0]) + run(bits[1]);
         }
 
-        // boolean Plus = exp.contains("+"); // contain : 감싸다
+//         boolean Plus = exp.contains("+"); // contain : 감싸다
+
+        if (exp.contains("(") && exp.contains(")")) {
+
+            exp = exp.replace("(", "");
+            exp = exp.replace(")", "");
+
+            if(plus) {
+                String[] bits = exp.split(" \\+ ");
+                int sum = 0;
+
+                for (int i = 0; i < bits.length; i++) {
+                    // bits의 길이가 i보다 클떄까지 i가 1씩 늘어나는 걸 반복
+                    sum += Integer.parseInt(bits[i]);
+                    // 그래서 그 배열만큼 sum에 추가
+
+                }
+
+                return sum;
+            }
+         }
 
         if (plus) {
 
@@ -58,7 +79,9 @@ public class Calc {
 
             return sum;
 
-        } else if (multi) {
+        }
+
+        else if (multi) {
 
             String[] bits = exp.split(" \\* ");
 
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -79,4 +79,36 @@ public class CalcTest {
         assertThat(Calc.run("((20 + 20)) * 20")).isEqualTo(420);
     }
 
+    @Test
+    @DisplayName("(10 + 20) == 30")
+    public void test13() {
+        assertThat(Calc.run("(10 + 20)")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("((10 + 20)) == 30")
+    public void test14() {
+        assertThat(Calc.run("((10 + 20))")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("(((10 + 20))) == 30")
+    public void test15() {
+        assertThat(Calc.run("(((10 + 20)))")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("((((10 + 20)))) == 30")
+    public void test16() {
+        assertThat(Calc.run("((((10 + 20))))")).isEqualTo(30);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) * 3 == 90")
+    public void test17() {
+        assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
+    }
+
+
+
 }
