9323c3415f838444c6c8c0b935a4bc90b263280c
yeji-star <jjjsk134@gmail.com>
2025-03-19 16:41:43 +0900

    03_19

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -6,9 +6,9 @@ import java.util.stream.Collectors;
 public class Calc {
 
     public static int run(String exp) {
-// 괄호 제거
+        // 괄호 제거
         exp = stripOuterBrackets(exp);
-//단일항 들어오면 리턴
+         //단일항 들어오면 리턴
         if (!exp.contains(" ")) {
             return Integer.parseInt(exp);
         }
@@ -21,6 +21,48 @@ public class Calc {
         boolean multi = exp.contains("*");
 
         boolean com = plus && multi;
+
+        if (exp.contains("(") && exp.contains(")")) {
+
+            if (exp.contains("*")) {
+                
+                exp = exp.replace("(", "");
+                exp = exp.replace(")", "");
+
+                String[] bits = exp.split(" \\* ");
+                int sum = 0;
+                if (exp.contains("+")) {
+                    //여기에 들어오는 건 10 + 20과 3
+
+                    for (int i = 0; i < bits.length; i++) {
+                        // bits의 길이가 i보다 클떄까지 i가 1씩 늘어나는 걸 반복
+                        sum += Integer.parseInt(bits[i]);
+                        // 그래서 그 배열만큼 sum에 추가
+                    }
+                    sum *= Integer.parseInt(bits[1]);
+                }
+                // bits[0] 안에 10 + 20이 존재
+
+                return sum;
+
+            }
+            exp = exp.replace("(", "");
+            exp = exp.replace(")", "");
+
+            if (plus) {
+                String[] bits = exp.split(" \\+ ");
+                int sum = 0;
+
+                for (int i = 0; i < bits.length; i++) {
+                    // bits의 길이가 i보다 클떄까지 i가 1씩 늘어나는 걸 반복
+                    sum += Integer.parseInt(bits[i]);
+                    // 그래서 그 배열만큼 sum에 추가
+
+                }
+
+                return sum;
+            }
+        }
 
         if (com) {
 
@@ -46,25 +88,6 @@ public class Calc {
 
 //         boolean Plus = exp.contains("+"); // contain : 감싸다
 
-        if (exp.contains("(") && exp.contains(")")) {
-
-            exp = exp.replace("(", "");
-            exp = exp.replace(")", "");
-
-            if (plus) {
-                String[] bits = exp.split(" \\+ ");
-                int sum = 0;
-
-                for (int i = 0; i < bits.length; i++) {
-                    // bits의 길이가 i보다 클떄까지 i가 1씩 늘어나는 걸 반복
-                    sum += Integer.parseInt(bits[i]);
-                    // 그래서 그 배열만큼 sum에 추가
-
-                }
-
-                return sum;
-            }
-        }
 
         if (plus) {
 
@@ -97,10 +120,15 @@ public class Calc {
     }
 
     private static String stripOuterBrackets(String exp) {
-        if (exp.charAt(0) == '(' && exp.charAt(exp.length() - 1) == ')') {
-            exp = exp.substring(1, exp.length() - 1);
+
+        int outerBracketsCount = 0;
+
+        while (exp.charAt(outerBracketsCount) == '(' && exp.charAt(exp.length() - 1 - outerBracketsCount) == ')') {
+            outerBracketsCount++;
         }
-        return exp;
+        if (outerBracketsCount == 0) return exp;
+
+        return exp.substring(outerBracketsCount, exp.length() - outerBracketsCount);
     }
 }
 
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -104,8 +104,14 @@ public class CalcTest {
     }
 
     @Test
-    @DisplayName("(10 + 20) * 3 == 90")
+    @DisplayName("(20 + 20) + 20 == 60")
     public void test17() {
+        assertThat(Calc.run("(20 + 20) + 20")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) * 3 == 90")
+    public void test18() {
         assertThat(Calc.run("(10 + 20) * 3")).isEqualTo(90);
     }
 
