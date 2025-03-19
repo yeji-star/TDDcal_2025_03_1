bb08c30e5290cbde84de9d4b9a222530c5b04739
yeji-star <jjjsk134@gmail.com>
2025-03-19 09:47:28 +0900

    03_18

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -7,31 +7,31 @@ public class Calc {
         boolean plus = exp.contains("+");
         boolean multi = exp.contains("*");
 
-        exp = exp.replace("- ", "+ -");
+        boolean com = plus && multi;
 
-        // boolean Plus = exp.contains("+"); // contain : 감싸다
-        if (plus) {
-
+        if (com) {
             String[] bits = exp.split(" \\+ ");
 
+            return Integer.parseInt(bits[0]) + run(bits[1]);
+        }
+
+        // boolean Plus = exp.contains("+"); // contain : 감싸다
+
+        if (plus) {
+            exp = exp.replace("- ", "+ -");
+
+            String[] bits = exp.split(" \\+ ");
+            int sum = 0;
 
             for (int i = 0; i < bits.length; i++) {
                 // bits의 길이가 i보다 클떄까지 i가 1씩 늘어나는 걸 반복
-
-                int sum = 0;
-
-                sum = Integer.parseInt(bits[i]);
+                sum += Integer.parseInt(bits[i]);
                 // 그래서 그 배열만큼 sum에 추가
-                if (plus) {
-                    sum += Integer.parseInt(bits[i]);
-                } else if (multi) {
-                    sum *= Integer.parseInt(bits[i]);
-                }
 
-                return sum;
+
             }
 
-
+            return sum;
         } else if (multi) {
 
             String[] bits = exp.split(" \\* ");
@@ -44,7 +44,6 @@ public class Calc {
 
             return sum;
         }
-
 
 
         throw new RuntimeException("실행 불가");
