fb66005b1a5dd5991981b5637f5b0be14dce84ec
yeji-star <jjjsk134@gmail.com>
2025-03-20 11:38:12 +0900

    03_20

--- src/main/java/org/example/Calc.java
+++ src/main/java/org/example/Calc.java
@@ -9,14 +9,23 @@ public class Calc {
 
         exp = exp.trim(); // 양 옆의 쓸데없는 공백 제거
         exp = stripOuterBrackets(exp); // 괄호 제거
-        //단일항 들어오면 리턴
+
+
+        if(isCaseMinusBracket(exp)) {
+            //만약에 -( 패턴이면, 내가 갖고 있는 코드는 해석 불가이니, 해석 가능하게
+            exp = exp.substring(1) + " * -1";
+        }
+
         if (!exp.contains(" ")) {
+            //단일항 들어오면 리턴
             return Integer.parseInt(exp);
         }
 
+        //안에 이런 부호들이 있나 확인
         boolean plus = exp.contains(" + ") || exp.contains(" - ");
         boolean multi = exp.contains(" * ");
         boolean Split = exp.contains("(") | exp.contains(")");
+        boolean minus = exp.contains("-");
         boolean com = plus && multi;
 
 
@@ -59,6 +68,7 @@ public class Calc {
 
                 exp = exp.replace("- ", "+ -");
 
+
                 String[] bits = exp.split(" \\+ ");
 
                 int sum = 0;
@@ -87,6 +97,13 @@ public class Calc {
 
             throw new RuntimeException("실행 불가");
         }
+
+    private static boolean isCaseMinusBracket(String exp) {
+        // -( 로 시작해?
+        if(exp.startsWith("-(") == false) return false; // -(로 시작안해?? 그럼 돌아가~~
+//startWith : 이 문자열로 시작하는지 안하는지
+        return true;
+    }
 
     private static int findSplitPointIndex(String exp) {
         int index = findSplitPointIndexBy(exp, '+'); // 하청 맡김 "쪼갤 수 있는 더하기 찾아주셈요"
--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -121,6 +121,12 @@ public class CalcTest {
         assertThat(Calc.run("10 + (10 + 5)")).isEqualTo(25);
     }
 
+    @Test
+    @DisplayName("-(10 + 5) == -15")
+    public void test20() {
+        assertThat(Calc.run("-(10 + 5)")).isEqualTo(-15);
+    }
+
 
 
 }
